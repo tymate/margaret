cb4e94af11ae2c09e23c7e807db89bb169441297
Henri Michel <henri@tymate.com>
2020-12-16 11:04:25 +0100

    feat: export main nav component

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
-export { default as Button } from './Button';
-export { default as ConfirmButton } from './ConfirmButton';
-export { default as MargaretProvider } from './MargaretProvider';
-export * from './MargaretProvider';
-export { default as Spinner } from './Spinner';
-export { default as Modal } from './Modal';
-export { default as Card } from './Card';
-export { default as Dropdown } from './Dropdown';
 export { default as Avatar } from './Avatar';
-export * from './Basics';
-// export { default as DataTable } from './DataTable';
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as ConfirmButton } from './ConfirmButton';
+export { default as Dropdown } from './Dropdown';
+export { default as MainNav } from './MainNav';
+export { default as MargaretProvider } from './MargaretProvider';
+export { default as Modal } from './Modal';
 export { default as SegmentedControls } from './SegmentedControls';
+export { default as Spinner } from './Spinner';
+
+export * from './Basics';
+export * from './MargaretProvider';
 export * from './Pills';
+// export { default as DataTable } from './DataTable';
 // export * from './Layout';
