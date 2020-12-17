3c5a9dbb6f383321862d82dbe8b3e15aa6ede6b7
Henri Michel <henri@tymate.com>
2020-12-17 17:01:00 +0100

    feat: expose section panel state hook

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as ConfirmButton } from './ConfirmButton';
 export { default as Dropdown } from './Dropdown';
-export { default as MainNav } from './MainNav';
+export { default as MainNav, useSectionPanel } from './MainNav';
 export { default as MargaretProvider } from './MargaretProvider';
 export { default as Modal } from './Modal';
 export { default as SegmentedControls } from './SegmentedControls';
