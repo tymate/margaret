5543ecee9a630bc401f108406483cecfd894c01b
Henri Michel <henri@tymate.com>
2021-01-29 09:51:45 +0100

    chore: bump storybook version
    * doc: add intro section, add sections & stories order
    * bump storybook to 6.1.15
    * remove unused decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { addDecorator, addParameters } from '@storybook/react';
-import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs } from '@storybook/addon-knobs';
 import { MargaretProvider } from '../src';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
@@ -30,21 +28,22 @@ addParameters({
   options: {
     showRoots: true,
     showCanvas: false,
+    storySort: {
+      order: ['Intro', ['Getting Started', 'Basics']],
+    },
   },
   dependencies: {
-    //display only dependencies/dependents that have a story in storybook
-    //by default this is false
+    // display only dependencies/dependents that have a story in storybook
+    // by default this is false
     withStoriesOnly: false,
 
-    //completely hide a dependency/dependents block if it has no elements
-    //by default this is false
+    // completely hide a dependency/dependents block if it has no elements
+    // by default this is false
     hideEmpty: false,
     previewTabs: { canvas: { hidden: true } },
   },
 });
 
-addDecorator(withA11y);
-addDecorator(withKnobs);
 addDecorator(story => (
   <>
     <MargaretProvider>
