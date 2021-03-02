a4c02af8212767bdc2986ee14ba80ab54e6db915
Henri Michel <henri@tymate.com>
2021-03-02 22:06:18 +0100

    fix: fix disabled button states

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,32 +8,27 @@ const ButtonWrapper = styled(ButtonReset)`
   position: relative;
   display: flex;
   white-space: nowrap;
-  transition: ${({ theme }) => theme.button.transition};
-  align-items: ${({ theme }) => theme.button.alignY};
-  justify-content: ${({ theme }) => theme.button.alignX};
-  border-radius: ${({ theme }) => theme.button.borderRadius};
-  padding: ${({ theme }) => theme.button.paddingHorizontal}
-    ${({ theme }) => theme.button.paddingHorizontal};
+  transition: ${({ theme }) => theme.button?.transition};
+  align-items: ${({ theme }) => theme.button?.alignY};
+  justify-content: ${({ theme }) => theme.button?.alignX};
+  border-radius: ${({ theme }) => theme.button?.borderRadius};
+  padding: ${({ theme }) => theme.button?.paddingHorizontal}
+    ${({ theme }) => theme.button?.paddingHorizontal};
   line-height: 1;
-  font-weight: ${({ theme }) => theme.button.fontWeight};
-  font-size: ${({ theme }) => theme.button.fontSize};
-  color: ${({ theme }) => theme.button.color};
-  min-width: ${({ theme }) => theme.button.minWidth};
-  box-shadow: ${({ theme }) => theme.button.boxShadow};
+  font-weight: ${({ theme }) => theme.button?.fontWeight};
+  font-size: ${({ theme }) => theme.button?.fontSize};
+  color: ${({ theme }) => theme.button?.color};
+  min-width: ${({ theme }) => theme.button?.minWidth};
+  box-shadow: ${({ theme }) => theme.button?.boxShadow};
 
   &:hover {
-    color: ${({ theme }) => theme.button.colorHover};
-    box-shadow: ${({ theme }) => theme.button.boxShadowHover};
+    color: ${({ theme }) => theme.button?.colorHover};
+    box-shadow: ${({ theme }) => theme.button?.boxShadowHover};
   }
 
   &:focus {
-    color: ${({ theme }) => theme.button.colorFocus};
-    box-shadow: ${({ theme }) => theme.button.boxShadowFocus};
-  }
-
-  &:disabled {
-    color: ${({ theme }) => theme.button.colorDisabled};
-    box-shadow: ${({ theme }) => theme.button.boxShadowDisabled};
+    color: ${({ theme }) => theme.button?.colorFocus};
+    box-shadow: ${({ theme }) => theme.button?.boxShadowFocus};
   }
 
   > svg {
@@ -50,70 +45,69 @@ const ButtonWrapper = styled(ButtonReset)`
       border-radius: ${({ theme }) =>
         theme.button?.[variant]?.borderRadius || theme?.button?.borderRadius};
       box-shadow: ${({ theme }) =>
-        theme.button?.[variant]?.boxShadow || theme.button.boxShadow};
+        theme.button?.[variant]?.boxShadow || theme.button?.boxShadow};
       background: ${({ theme }) =>
-        theme.button?.[variant]?.background || theme.button.background};
+        theme.button?.[variant]?.background || theme.button?.background};
       color: ${({ theme }) =>
-        theme.button?.[variant]?.color || theme.button.color};
+        theme.button?.[variant]?.color || theme.button?.color};
       padding: ${({ theme }) =>
         theme.button?.[variant]?.paddingVertical ||
-        theme.button.paddingVertical}
+        theme.button?.paddingVertical}
         ${({ theme }) =>
           theme.button?.[variant]?.paddingHorizontal ||
-          theme.button.paddingHorizontal};
+          theme.button?.paddingHorizontal};
       min-width: ${({ theme }) =>
-        theme.button?.[variant]?.minWidth || theme.button.minWidth}
-        || ${({ theme }) => theme.button.minWidth};
+        theme.button?.[variant]?.minWidth || theme.button?.minWidth}
+        || ${({ theme }) => theme.button?.minWidth};
 
       &:hover {
         background: ${({ theme }) =>
           theme.button?.[variant]?.backgroundHover ||
           theme.button?.[variant]?.background ||
-          theme.button.backgroundHover};
+          theme.button?.backgroundHover};
         color: ${({ theme }) =>
           theme.button?.[variant]?.colorHover ||
           theme.button?.[variant]?.color ||
-          theme.button.colorHover};
+          theme.button?.colorHover};
         box-shadow: ${({ theme }) =>
           theme.button?.[variant]?.boxShadowHover ||
           theme.button?.[variant]?.boxShadow ||
-          theme.button.boxShadowHover};
+          theme.button?.boxShadowHover};
         transform: ${({ theme }) =>
           theme.button?.[variant]?.transformHover ||
-          theme.button.transformHover};
+          theme.button?.transformHover};
       }
 
       &:focus {
         background: ${({ theme }) =>
           theme.button?.[variant]?.backgroundFocus ||
           theme.button?.[variant]?.background ||
-          theme.button.backgroundFocus};
+          theme.button?.backgroundFocus};
         color: ${({ theme }) =>
           theme.button?.[variant]?.colorFocus ||
           theme.button?.[variant]?.color ||
-          theme.button.colorFocus};
+          theme.button?.colorFocus};
         box-shadow: ${({ theme }) =>
           theme.button?.[variant]?.boxShadowFocus ||
           theme.button?.[variant]?.boxShadow ||
-          theme.button.boxShadowFocus};
+          theme.button?.boxShadowFocus};
         transform: ${({ theme }) => theme.button?.[variant]?.transformFocus} ||
-          theme.button.transformFocus};
+          theme.button?.transformFocus};
       }
       
 
       &:disabled {
         background: ${({ theme }) =>
           theme.button?.[variant]?.backgroundDisabled ||
-          theme.button?.[variant]?.background ||
-          theme.button.backgroundDisabled};
+          theme.button?.backgroundDisabled ||
+          theme.backgroundDisabled};
         color: ${({ theme }) =>
           theme.button?.[variant]?.colorDisabled ||
-          theme.button?.[variant]?.color ||
-          theme.button.colorDisabled};
+          theme.button?.colorDisabled ||
+          theme.textDisabled};
         box-shadow: ${({ theme }) =>
           theme.button?.[variant]?.boxShadowDisabled ||
-          theme.button?.[variant]?.boxShadow ||
-          theme.button.boxShadowDisabled};
+          theme.button?.boxShadowDisabled};
         transform: ${({ theme }) => theme.button?.[variant]?.transformDisabled};
       }
     `}
@@ -138,9 +132,9 @@ const ButtonWrapper = styled(ButtonReset)`
 
   &:disabled {
     cursor: not-allowed;
-    background: ${({ theme }) => theme.button.backgroundDisabled};
-    color: ${({ theme }) => theme.button.colorDisabled};
-    box-shadow: ${({ theme }) => theme.button.boxShadowDisabled};
+    background: ${({ theme }) => theme.button?.backgroundDisabled};
+    color: ${({ theme }) => theme.button?.colorDisabled};
+    box-shadow: ${({ theme }) => theme.button?.boxShadowDisabled};
   }
 `;
 
